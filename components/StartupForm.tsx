e40755c22dbeacf0a573783174c3c1264acff24f
LinesnEffects <bgf2311@outlook.com>
2025-04-12 11:49:01 -0500

    Completed most of startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
 
 const StartupForm = () => {
-  return (
-    <div>StartupForm</div>
-  )
-}
+  const [errors, setErrors] = useState<Record<string, string>>({});
 
-export default StartupForm
+  return (
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+      
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Input
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup description"
+        />
+        {errors.description && <p className="startup-form_error">{errors.description}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category (Tech, Education, Health ...)"
+        />
+        {errors.category && <p className="startup-form_error">{errors.category}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          required
+          placeholder="Startup Image URL"
+        />
+        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
+
+    </form>
+  );
+};
+
+export default StartupForm;
