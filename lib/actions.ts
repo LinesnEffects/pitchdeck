ba342b87dff215c42e13a6adccfc516c4f4131d2
LinesnEffects <bgf2311@outlook.com>
2025-04-18 15:58:48 -0500

    Finished server actions, discarded sentry commit

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth";
 import { parseServerActionResponse } from "@/lib/utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const createPitch = async (
   state: any,
@@ -19,4 +21,39 @@ export const createPitch = async (
   const { title, description, category, link } = Object.fromEntries(
     Array.from(form).filter(([key]) => key !== "pitch")
   );
+
+  const slug = slugify(title as string, { lower: true, strict: true });
+
+  try {
+    const startup = {
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: slug,
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session?.id,
+      },
+      pitch,
+    };
+
+    const result = await writeClient.create({ _type: "startup", ...startup });
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
 };
