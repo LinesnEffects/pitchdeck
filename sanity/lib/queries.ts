58153f9bce69dfdbc708085b782d8c72f0d0f5eb
LinesnEffects <bgf2311@outlook.com>
2025-04-23 11:15:43 -0500

    Rendered startups playlist on pitch details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search ] | order(_createdAt desc) {
+export const STARTUPS_QUERY = defineQuery(
+  `*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search ] | order(_createdAt desc) {
     _id,
     title,
     slug,
@@ -15,8 +15,8 @@ export const STARTUPS_QUERY =
     image
   }`);
 
-export const STARTUP_BY_ID_QUERY =
-  defineQuery(`*[_type == "startup" && _id == $id][0]{
+export const STARTUP_BY_ID_QUERY = defineQuery(
+  `*[_type == "startup" && _id == $id][0]{
   _id,
   title,
   slug,
@@ -45,8 +45,7 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
     image, 
     bio
     }
-  `
-);
+  `);
 
 export const AUTHOR_BY_ID_QUERY = defineQuery(
   `*[_type == "author" && _id == $id][0]{
@@ -61,8 +60,8 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(
   `
 );
 
-export const STARTUPS_BY_AUTHOR_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current) && author._ref == $id ] | order(_createdAt desc) {
+export const STARTUPS_BY_AUTHOR_QUERY = defineQuery(
+  `*[_type == "startup" && defined(slug.current) && author._ref == $id ] | order(_createdAt desc) {
     _id,
     title,
     slug,
@@ -74,4 +73,29 @@ export const STARTUPS_BY_AUTHOR_QUERY =
     description,
     category,
     image
+  }`);
+
+export const PLAYLIST_BY_SLUG_QUERY = defineQuery(
+  `*[_type == "playlist" && slug.current == $slug][0]{
+    _id,
+    title,
+    slug,
+    select[]->{
+      _id,
+      _createdAt,
+      title,
+      slug,
+      author->{
+        _id,
+        name,
+        slug,
+        image,
+        bio
+      },
+      views,
+      description,
+      category,
+      image,
+      pitch
+    }
   }`);
